cb1b599811e1c0a3255a682f49a7e6388752ade8
GeneChatham <genechatham@gmail.com>
2013-11-29 23:31:20 -0600

    Basic hash-clash working on local host.  Just added Procfile for Heroku deployment.  Not tested on Heroku yet.

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,23 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'twitter'
+
+
+# Creating intense-river-7310... done, stack is cedar
+# http://intense-river-7310.herokuapp.com/ | git@heroku.com:intense-river-7310.git
+# Git remote heroku added
 
 get '/' do
-  'hello world'
+	@names = "Emma and Gene"
+	@show_description = true
+  erb  :index
+end
+
+post '/twitter' do
+
+ @firstHash = params[:first_hash]
+ @secondHash = params[:second_hash]
+ erb :results
+
 end
